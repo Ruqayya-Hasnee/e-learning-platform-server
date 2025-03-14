594fd412e4e8417bef256a486239f5367dbede2c
Student-Ruqayya <ruqayya.hasnee@gmail.com>
2025-03-14 22:05:10 +0500

    added jwt

--- src/modules/Auth/auth.module.ts
+++ src/modules/Auth/auth.module.ts
@@ -3,12 +3,17 @@ import { AuthController } from './auth.controller';
 import { UserModule } from '../User/user.module';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [UserModule,
-    JwtModule.register({
-      secret: 'adada', // Replace with a strong secret
-      signOptions: { expiresIn: '1h' }, // Token expiry time
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') },
+      }),
     }),
   ],
   controllers: [AuthController],
