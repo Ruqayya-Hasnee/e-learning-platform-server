5875e068610cb5268cdf351b9fec4594f8849c08
Student-Ruqayya <ruqayya.hasnee@gmail.com>
2025-03-14 21:57:20 +0500

    changes

--- src/modules/Auth/auth.module.ts
+++ src/modules/Auth/auth.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../User/user.module';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule,
+    JwtModule.register({
+      secret: 'adada', // Replace with a strong secret
+      signOptions: { expiresIn: '1h' }, // Token expiry time
+    }),
+  ],
   controllers: [AuthController],
   providers:[AuthService]
 })
--- src/modules/Auth/auth.service.ts
+++ src/modules/Auth/auth.service.ts
@@ -1,12 +1,20 @@
-import { Injectable, ConflictException, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UserService } from '../User/user.service';
 import { CreateUserDto } from '../User/dto/create-user.dto';
 import { User } from '../User/entities/user.entity';
 import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async signup(createUserDto: CreateUserDto): Promise<User> {
     const existingUser = await this.userService.findUserByEmail(
@@ -18,17 +26,28 @@ export class AuthService {
     return this.userService.createUser(createUserDto);
   }
 
-
-  async login(loginDto: LoginDto): Promise<User> {
+  async login(loginDto: LoginDto): Promise<{ access_token: string }> {
     const existingUser = await this.userService.findUserBy({
       email: loginDto.email,
-      password: loginDto.password
+      password: loginDto.password,
     });
 
     if (!existingUser) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Invalid email or password');
     }
 
-    return existingUser
+    // Generate JWT token
+    const payload = { userId: existingUser.id, email: existingUser.email };
+    const access_token = this.jwtService.sign(payload);
+
+    return { access_token };
+  }
+
+  verifyToken(token: string) {
+    try {
+      return this.jwtService.verify(token);
+    } catch (e) {
+      throw new UnauthorizedException('Invalid or expired token');
+    }
   }
 }
